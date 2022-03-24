0d9cca5bd4d85fcd27369151a56b6b089324554f
jafman <kamsy74@gmail.com>
2022-03-24 14:33:42 +0100

    updates contracts

--- contracts/TrothToken.sol
+++ contracts/TrothToken.sol
@@ -2,19 +2,20 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/roles/MinterRole.sol";
+// import "@openzeppelin/contracts/access/roles/MinterRole.sol";
 
-/**
- * @dev Extension of {ERC20} that adds a set of accounts with the {MinterRole},
- * which have permission to mint (create) new tokens as they see fit.
- *
- * At construction, the deployer of the contract is the only minter.
- */
-contract TrothToken is ERC20, MinterRole {
+contract TrothToken is ERC20 {
 
     string private _name;
     string private _symbol;
     uint8 private _decimals;
+    uint256 private _derivedAmount;
+
+    // Map address to merchants
+    mapping (address => string) public merchants;
+
+    // wallet balance of customers mapped to merchants
+    mapping (address => mapping(address => uint256)) public walletBalance;
 
     constructor(
         string memory name,
@@ -41,31 +42,59 @@ contract TrothToken is ERC20, MinterRole {
         return _symbol;
     }
 
-    /**
-     * @dev Returns the number of decimals used to get its user representation.
-     * For example, if `decimals` equals `2`, a balance of `505` tokens should
-     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
-     *
-     * Tokens usually opt for a value of 18, imitating the relationship between
-     * Ether and Wei.
-     *
-     * NOTE: This information is only used for _display_ purposes: it in
-     * no way affects any of the arithmetic of the contract, including
-     * {IERC20-balanceOf} and {IERC20-transfer}.
-     */
     function decimals() public view returns (uint8) {
         return _decimals;
     }
 
-    /**
-     * @dev See {ERC20-_mint}.
-     *
-     * Requirements:
-     *
-     * - the caller must have the {MinterRole}.
-     */
-    function mint(address account, uint256 amount) public onlyMinter returns (bool) {
-        _mint(account, amount);
+    function mint(address account, uint256 amount, address merchant) public returns (bool) {
+        // require that merchant exist
+        require(merchants[merchant] != false, "Onboarding: merchant does not exist");
+        _derivedAmount = amount * (uint256(10) ** _decimals);
+        _mint(account, _derivedAmount);
+        _updateWalletBalance(merchant, account, amount, 'add');
         return true;
+    }
+
+    function spend(address account, uint256 amount, address merchant) public returns (bool) {
+        _derivedAmount = amount * (uint256(10) ** _decimals);
+        _burn(account, _derivedAmount);
+        _updateWalletBalance(merchant, account, amount, 'sub');
+        return true;
+    }
+
+    function getBalance(address account, address merchant) public view returns (uint256) {
+        return walletBalance[merchant][account];
+    }
+
+    // Onboard new merchant
+    function onboardMerchant(string name) {
+        _addMerchant(msg.sender, name);
+    }
+
+    //get merchants
+    function getMerchants() public view returns (string[]) {
+        return merchants;
+    }
+
+    // add new merchant
+    function _addMerchant(address merchant, string name) public {
+        require(merchants[merchant] == false, "Onboarding: merchant already exists");
+        require(name != "", "Merchant: name is empty");
+        merchants[merchant] = name;
+    }
+
+    // update customer wallet balance
+    function _updateWalletBalance(address merchant, address customer, uint256 amount, string operation) public {
+        require(merchants[merchant] != false, "Merchant not found");
+        require(customer != 0x0, "Customer address is empty");
+        require(amount != 0, "Amount is empty");
+
+        if(operation == "add") {
+            walletBalance[merchant][customer] += amount;
+        } else if(operation == "sub") {
+            walletBalance[merchant][customer] -= amount;
+        } else {
+            require(false, "Operation is not valid");
+        }
     }
 }
